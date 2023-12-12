efc2420ed66615a55f22ef35febf5401d8de3a71
Joshua Burke <jburke@crosswind.com>
2023-12-12 00:30:06 -0500

    fix(webpack): update webpack configuration to use terser-webpack-plugin
    The terser-webpack-plugin is the successor to the uglifyjs-webpack-plugin
    and is the recommended plugin for minifying JavaScript. It is more actively
    maintained and has better support for ES6+ syntax.
    
    Update the babel-loader to be more specific about the version of browsers
    that we support. This will allow the terser-webpack-plugin to better
    optimize the output.
    
    BREAKING CHANGE: The uglifyjs-webpack-plugin is no longer used. The
    terser-webpack-plugin is now used instead.

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -9,7 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
 const { default: mi18n } = require('mi18n')
 const { languageFiles, enUS } = require('formeo-i18n')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const { IS_PRODUCTION, ANALYZE, projectRoot, outputDir, devtool, bannerTemplate, version } = require('./build-vars')
@@ -153,11 +153,22 @@ const webpackConfig = {
   plugins,
   devtool,
   optimization: {
+    minimize: !!IS_PRODUCTION,
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        exclude: /\/node_modules/,
         parallel: true,
         sourceMap: !IS_PRODUCTION,
+        terserOptions: {
+          ecma: 5,
+          compress: {
+            drop_console: true,
+          },
+          output: {
+            comments: false,
+          },
+        },
       }),
       new OptimizeCSSAssetsPlugin(),
     ],
